2043cb620013bd80c22d9703e514e4f7bab82590
Karishma Patel <karishma1126@yahoo.com>
2013-10-29 09:08:30 -0600

    started cards

---
+++ Card.java
@@ -0,0 +1,15 @@
+public class Card {
+
+	private int number;
+	private String suit;
+
+	public Card(String suit, int number) {
+		Card spades2 = new Card ("Spades", 2);
+		Card 
+
+	}
+
+	public voi
+
+
+}
