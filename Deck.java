6f439b48381dfc8df955bed6c6637c94dcc78bdd
patelkd <karishma1126@yahoo.com>
2013-10-31 09:37:11 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,26 +1,46 @@
 public class Deck {
 
-	public static final int card = 52;
-	public static int[] stack;
+    private String suit;
+    private int value;
+    private int value2
+    private int[] deck;
 
-	public Deck(int cards) {
-		stack = new int[52];
-
+    public Deck(String suit, int value) {
+        for (int i=0; i<13; i++) {
+            int x = i
+            }
+            Card card = new Card ("Diamonds", i);
+            
+        }
     }
 
-	public static void main(String[] args) {
-		for (int i=0; i<51; i++) {
-			System.out.println(card);
-		}
-	}
+
+
+
+
+// public class Deck {
+
+// 	public static final int card = 52;
+// 	public static int[] stack;
+
+// 	public Deck(int cards) {
+// 		stack = new int[52];
+
+//     }
+
+// 	public static void main(String[] args) {
+// 		for (int i=0; i<51; i++) {
+// 			System.out.println(card);
+// 		}
+// 	}
 		
 
-		// for (int suit = 0; rank<=Card.SPADE; suit++) {
-		// 	for (int rank=1; rank<=13; rank++) {
-		// 		deck[i++] = new Card(suit, rank);
-		// 		System.out.println(rank + "of" + suit);
-		// 	}
-		// }
+// 		// for (int suit = 0; rank<=Card.SPADE; suit++) {
+// 		// 	for (int rank=1; rank<=13; rank++) {
+// 		// 		deck[i++] = new Card(suit, rank);
+// 		// 		System.out.println(rank + "of" + suit);
+// 		// 	}
+// 		// }
 
 
-}
+// }
