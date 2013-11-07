85618b21b418ce36347cc948c01b570a1f3f84f9
patelkd <karishma1126@yahoo.com>
2013-11-06 22:06:53 -0700

    Print and Shuffle Cards

--- Deck.java
+++ Deck.java
@@ -1,18 +1,46 @@
 public class Deck {
+	
+ public static void main(String[] args) {
+    int[] deck = new int[52];
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-    private String suit;
-    private int value;
-    private int value2
-    private int[] deck;
-
-    public Deck(String suit, int value) {
-        for (int i=0; i<13; i++) {
-            int x = i
-            }
-            Card card = new Card ("Diamonds", i);
-            
-        }
+    // Initialize cards
+    for (int i = 0; i < deck.length; i++) {
+      deck[i] = i;
     }
+
+    // Shuffle the cards
+    for (int i = 0; i < deck.length; i++) {
+      int index = (int)(Math.random() * deck.length);
+      int temp = deck[i];
+      deck[i] = deck[index];
+      deck[index] = temp;
+    }
+
+    // Display the all the cards
+    for (int i = 0; i < 52; i++) {
+      String suit = suits[deck[i] / 13];
+      String rank = ranks[deck[i] % 13];
+      System.out.println( rank + " of " + suit);
+    }
+  }
+
+
+
+    //private String suit;
+    //private int value;
+    //private int value2
+    //private int[] deck;
+
+    //public Deck(String suit, int value) {
+    //    for (int i=0; i<13; i++) {
+    //        int x = i
+    //        }
+    //        Card card = new Card ("Diamonds", i);
+            
+    //    }
+    //}
 
 
 
