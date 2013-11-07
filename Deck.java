ac9f7b3af0db99acd204595922440dfd5ccae8aa
patelkd <karishma1126@yahoo.com>
2013-11-07 09:32:20 -0700

    Corrected Deck

--- Deck.java
+++ Deck.java
@@ -1,74 +1,66 @@
+import java.util.Random;
+
 public class Deck {
-	
- public static void main(String[] args) {
-    int[] deck = new int[52];
-    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-    // Initialize cards
-    for (int i = 0; i < deck.length; i++) {
-      deck[i] = i;
+    private Card[] deck = new Card[52];
+    private String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    private String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private int value;
+
+
+
+    public Deck () {
+        int counter = 0;
+        String name;
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<13; j++) {
+                name = ranks[j] + suits[i];
+                if (j>0 && j<9) {
+                    value = j;
+                } else if (j>=9) {
+                    value = 10;
+                } else {
+                    value = 11;
+                }
+                this.deck[counter] = new Card(name, value);
+                counter++;
+            }
+        }
+
+        // for (int i = 0; i < deck.length; i++) {
+        //   deck[i] = i;
+        // }
     }
 
-    // Shuffle the cards
-    for (int i = 0; i < deck.length; i++) {
-      int index = (int)(Math.random() * deck.length);
-      int temp = deck[i];
-      deck[i] = deck[index];
-      deck[index] = temp;
+
+    public void shuffle () {
+        //shuffle
+        for (int i = 0; i < this.deck.length; i++) {
+          int index = (int)(Math.random() * this.deck.length);
+          Card temp = deck[i];
+          deck[i] = deck[index];
+          deck[index] = temp;
+        }
+    }
+    
+    public void print () {
+        //display cards
+        for (int i = 0; i < 52; i++) {
+          System.out.println(this.deck[i].name());
+        }
     }
 
-    // Display the all the cards
-    for (int i = 0; i < 52; i++) {
-      String suit = suits[deck[i] / 13];
-      String rank = ranks[deck[i] % 13];
-      System.out.println( rank + " of " + suit);
+      
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.print();
     }
-  }
+
+   
+
+
+}
 
 
 
-    //private String suit;
-    //private int value;
-    //private int value2
-    //private int[] deck;
-
-    //public Deck(String suit, int value) {
-    //    for (int i=0; i<13; i++) {
-    //        int x = i
-    //        }
-    //        Card card = new Card ("Diamonds", i);
-            
-    //    }
-    //}
-
-
-
-
-
-// public class Deck {
-
-// 	public static final int card = 52;
-// 	public static int[] stack;
-
-// 	public Deck(int cards) {
-// 		stack = new int[52];
-
-//     }
-
-// 	public static void main(String[] args) {
-// 		for (int i=0; i<51; i++) {
-// 			System.out.println(card);
-// 		}
-// 	}
-		
-
-// 		// for (int suit = 0; rank<=Card.SPADE; suit++) {
-// 		// 	for (int rank=1; rank<=13; rank++) {
-// 		// 		deck[i++] = new Card(suit, rank);
-// 		// 		System.out.println(rank + "of" + suit);
-// 		// 	}
-// 		// }
-
-
-// }
