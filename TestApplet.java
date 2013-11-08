15b1624cfd538e27a226abeeab257f5232aa7db2
patelkd <karishma1126@yahoo.com>
2013-11-07 17:30:18 -0700

    Update TestApplet.java

--- TestApplet.java
+++ TestApplet.java
@@ -3,14 +3,14 @@ import java.applet.*;
 
 public class TestApplet extends Applet {
 
-	private MoviePoster poster;
+        private Card card;
 
-	public void init() {
-		poster = new MoviePoster("Jurassic Park");
-	}
+        public void init() {
+                card = new Card("Deck", 10);
+        }
 
-	public void paint(Graphics g) {
-		poster.draw(g, new Rectangle(50, 50, 200, 300));
-	}
+        public void paint(Graphics g) {
+                card.draw(g, new Rectangle(50, 50, 200, 300));
+        }
 
 }
